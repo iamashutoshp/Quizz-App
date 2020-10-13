c2074eb8e21b90d2c3efd6e09d4882fee7510b68
iamashutoshp <ap1853177@gmail.com>
2020-10-13 21:39:37 +0530

    Quiz app v1.0

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -1,10 +1,13 @@
 package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,35 +19,53 @@ public class MainActivity extends Activity {
     Button mTrueButton;
     Button mFalseButton;
     TextView mQuestionTextView;
-    int mQuestions,mIndex;
+    int mQuestions,mIndex,mScore;
+
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
+
 
     // TODO: Uncomment to create question bank
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_9, true),
             new TrueFalse(R.string.question_1, true),
             new TrueFalse(R.string.question_2, true),
             new TrueFalse(R.string.question_3, true),
             new TrueFalse(R.string.question_4, true),
-            new TrueFalse(R.string.question_5, true),
             new TrueFalse(R.string.question_6, false),
             new TrueFalse(R.string.question_7, true),
             new TrueFalse(R.string.question_8, false),
-            new TrueFalse(R.string.question_9, true),
             new TrueFalse(R.string.question_10, true),
             new TrueFalse(R.string.question_11, false),
             new TrueFalse(R.string.question_12, false),
             new TrueFalse(R.string.question_13,true)
     };
-
+    final int PROGRESS_BAR_INCREMENT = (int)Math.ceil(100.0/mQuestionBank.length);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            mScore = savedInstanceState.getInt("Score");
+            mIndex = savedInstanceState.getInt("IndexKey");
+        }
+        else
+        {
+            mScore = 0;
+            mIndex = 0;
+        }
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
         mQuestionTextView = findViewById(R.id.question_text_view);
+        mScoreTextView = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
 
         mQuestions = mQuestionBank[mIndex].getQuestionId();
         mQuestionTextView.setText(mQuestions);
+
+        mScoreTextView.setText("Score : "+mScore+"/"+mQuestionBank.length);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,17 +85,41 @@ public class MainActivity extends Activity {
 
     private void UpdateQuestion(){
         mIndex=(mIndex+1)%(mQuestionBank.length);
+        if(mIndex == 0){
+            AlertDialog.Builder alert=new AlertDialog.Builder(this);
+            alert.setTitle("Game Over!");
+            alert.setCancelable(false);
+            alert.setMessage("Final Score : "+mScore+" Points");
+            alert.setPositiveButton("Close App", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
         mQuestions=mQuestionBank[mIndex].getQuestionId();
         mQuestionTextView.setText(mQuestions);
+        mScoreTextView.setText("Score : "+mScore+"/"+mQuestionBank.length);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+
     }
 
     private void CheckAnswer(boolean UserSelection){
         boolean correctAnswer=mQuestionBank[mIndex].isAnswer();
         if(correctAnswer && UserSelection){
             Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            mScore = mScore+1;
         }
         else{
             Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("Score",mScore);
+        outState.putInt("IndexKey",mIndex);
     }
 }
